cd6e37823e15a1292d3ca38231cf45ddfc211a09
Joel Rocha <joelxr@gmail.com>
2017-03-02 22:38:09 -0300

    finalizando aula 63

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 var app = require('./config/server');
 
-app.listen(8080, function () {
+var server = app.listen(8080, function () {
     console.log('servidor ONLINE');
+});
+
+var io = require('socket.io').listen(server);
+
+io.on('connection', function(socket){
+    console.log('usuario conectou');
+
+    socket.on('disconnect', function() {
+        console.log('usuario desconectou');
+    });
 });
