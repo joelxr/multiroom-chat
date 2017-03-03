c3da1196380445d249a5f2a133b7cb7711ab421f
Joel Rocha <joelxr@gmail.com>
2017-03-02 23:40:45 -0300

    adicionado mensagens

--- app.js
+++ app.js
@@ -6,10 +6,34 @@ var server = app.listen(8080, function () {
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 io.on('connection', function(socket){
     console.log('usuario conectou');
 
     socket.on('disconnect', function() {
         console.log('usuario desconectou');
+    });
+
+    socket.on('msgParaServidor', function(data) {
+        socket.emit(
+                'msgParaCliente',
+                {apelido: data.apelido, mensagem: data.mensagem}
+                );
+
+        socket.broadcast.emit(
+                'msgParaCliente',
+                {apelido: data.apelido, mensagem: data.mensagem}
+                );
+
+        socket.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+                );
+
+        socket.broadcast.emit(
+                'participantesParaCliente',
+                {apelido: data.apelido}
+                );
     });
 });
